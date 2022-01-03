98247b2d7fd4819ea5174c382ae45e7a7572fa88
ivankp <ivan.pogrebnyak@gmail.com>
2022-01-03 17:48:40 -0500

    switch shortcuts users

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const _id = id => document.getElementById(id);
-function make(p,...tags) {
+function $(p,...tags) {
   for (const t of tags)
     p = p.appendChild(document.createElement(t))
   return p;
@@ -26,6 +26,36 @@ function time(clock,date) {
   return 60000 - (ms + s*1000);
 }
 
+const shortcuts = {
+  'g': ['https://mail.google.com/mail/u/0/#inbox', 'gmail'],
+  'c': ['https://calendar.google.com/calendar/u/0/r/week', 'calendar'],
+  'd': ['https://docs.google.com/document/u/0/', 'documents'],
+  'l': ['https://www.linkedin.com/', 'linkedin'],
+  'f': ['https://www.facebook.com/', 'facebook'],
+  'm': ['https://www.facebook.com/messages/', 'messages'],
+  'h': ['https://github.com/ivankp?tab=repositories', 'github'],
+  's': ['https://stackoverflow.com/users/2640636', 'stackoverflow'],
+  'w': ['https://www.wolframalpha.com/', 'wolframalpha'],
+  'u': ['https://www.youtube.com/', 'youtube'],
+  'i': ['https://images.google.com/', 'google images'],
+  'z': ['https://zooescape.com/backgammon.pl?v=200&ng=1', 'zooescape'],
+};
+let u = '0';
+function make_shortcuts() {
+  const div = clear(_id('shortcuts'));
+  const t = $(div,'table');
+  const tr = $(t,'tr');
+  $(tr,'td').textContent = `[${u}]`;
+  $(tr,'td').textContent = 'user';
+  for (const [k,v] of Object.entries(shortcuts)) {
+    const tr = $(t,'tr');
+    $(tr,'td').textContent = `[${k}]`;
+    const a = $(tr,'td','a');
+    a.textContent = v[1];
+    a.href = v[0].replace(/\/u\/\d\//,`/u/${u}/`);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   { // time and date
     const clock = _id('clock');
@@ -41,6 +71,19 @@ document.addEventListener('DOMContentLoaded', () => {
   //   .then(data => { _id('motd').textContent = data; });
   // }
   { _id('browser').textContent
-      = navigator.userAgent.match(/(?<=^|\s)(Chrom\S+\/\S+)/)[1];
+      = navigator.userAgent;
+      // = navigator.userAgent.match(/(?<=^|\s)(Chrom\S+\/\S+)/)[1];
+  }
+  make_shortcuts();
+});
+
+document.addEventListener("keypress", e => {
+  const x = shortcuts[e.key];
+  if (x) window.location.href = x[0].replace(/\/u\/\d\//,`/u/${u}/`);
+  else if (['0','1'].includes(e.key)) {
+    if (e.key!==u) {
+      u = e.key;
+      make_shortcuts();
+    }
   }
 });
