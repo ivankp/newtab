368427ca7f350a665e912d189b42f684307023c4
ivankp <ivan.pogrebnyak@gmail.com>
2025-10-18 21:47:12 -0400

    big update

--- script.js
+++ script.js
@@ -1,111 +1,97 @@
-const _id = id => document.getElementById(id);
-function $(p,...tags) {
-  for (const t of tags)
-    p = p.appendChild(document.createElement(t))
+const $ = (p, ...args) => {
+  if (p.constructor === String) {
+    p = document.getElementById(p);
+  }
+  for (let x of args) {
+    if (x.constructor === String) {
+      p = p.appendChild( (p instanceof SVGElement || x==='svg')
+        ? document.createElementNS('http://www.w3.org/2000/svg', x)
+        : document.createElement(x)
+      );
+    // } else if (x.nodeType === Node.ELEMENT_NODE) {
+    //   p.appendChild(x);
+    } else if (x.constructor === Array) {
+      p.classList.add(...x);
+    } else if (x.constructor === Function) {
+      x(p);
+    } else if (x.constructor === Object) {
+      for (const [key,val] of Object.entries(x)) {
+        if (key==='style') {
+          for (const [k,v] of Object.entries(val)) {
+            if (v!==null) p.style[k] = v;
+            else p.style.removeProperty(k);
+          }
+        } else if (key==='events') {
+          for (const [k,v] of Object.entries(val)) {
+            if (v!==null) p.addEventListener(k,v);
+            else p.removeEventListener(k);
+          }
+        } else if (key==='text') {
+          p.textContent = val;
+        } else {
+          if (val!==null) {
+            if (p instanceof SVGElement)
+              p.setAttributeNS(null,key,val);
+            else
+              p.setAttribute(key,val);
+          } else {
+            if (p instanceof SVGElement)
+              p.removeAttributeNS(null,key);
+            else
+              p.removeAttribute(key);
+          }
+        }
+      }
+    }
+  }
   return p;
-}
-function clear(x) {
-  for (let c; c = x.firstChild; ) x.removeChild(c);
-  return x;
-}
+};
+const $$ = (...args) => p => $(p, ...args);
 
 const weekdays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-const months = [
-  'Jan','Feb','Mar','Apr','May','June','July','Aug','Sept','Oct','Nov','Dec'];
+const months = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec'];
 
-function time(clock,date) {
+var clock, date;
+const time = () => {
   const t = new Date();
   const ms = t.getMilliseconds();
   const s  = t.getSeconds();
   const m  = t.getMinutes();
   let   h  = t.getHours();
-  const p  = h>12 ? (h-=12, 'pm') : 'am';
+  const p  = h >= 12 ? (h -= 12, 'PM') : 'AM';
   if (h === 0) h = 12;
-  clock.textContent = `${h}:${m<10?'0':''}${m}${p}`;
+  clock.textContent = `${h}:${m<10?'0':''}${m} ${p}`;
   date .textContent =
     `${weekdays[t.getDay()]}, ${months[t.getMonth()]} ${t.getDate()}, ${t.getFullYear()}`;
   return 60000 - (ms + s*1000);
 }
 
-const shortcuts = {
-  'm': ['https://mail.google.com/mail/u/0/#inbox', 'gmail'],
-  'c': ['https://calendar.google.com/calendar/u/0/r/week', 'calendar'],
-  'd': ['https://docs.google.com/document/u/0/', 'documents'],
-  't': ['https://docs.google.com/spreadsheets/u/0/', 'tables'],
-  'l': ['https://www.linkedin.com/in/ivan-pogrebnyak/', 'linkedin'],
-  'F': ['https://www.facebook.com/', 'facebook'],
-  'f': ['https://www.facebook.com/messages/', 'messages'],
-  'g': ['https://github.com/ivankp', 'github'],
-  'G': ['https://github.com/ivankp?tab=repositories', 'github repos'],
-  's': ['https://stackoverflow.com/users/2640636', 'stackoverflow'],
-  'w': ['https://www.wolframalpha.com/', 'wolframalpha'],
-  'u': ['https://www.youtube.com/', 'youtube'],
-  'U': ['https://www.youtube.com/feed/history', 'youtube history'],
-  'i': ['https://images.google.com/', 'google images'],
-  'M': ['https://www.google.com/maps', 'google maps'],
-  'a': ['https://www.amazon.com/', 'amazon'],
-  'z': ['https://zooescape.com/backgammon.pl?v=200&ng=1', 'zooescape'],
-  'h': ['https://home.nest.com', 'nest'],
-};
-let u = '0';
-function make_shortcuts() {
-  const div = clear(_id('shortcuts'));
-  const t = $(div,'table');
-  const tr = $(t,'tr');
-  $(tr,'td').textContent = `[${u}]`;
-  $(tr,'td').textContent = 'user';
+const make_shortcuts = () => {
+  const table = $('shortcuts','table');
   for (const [k,v] of Object.entries(shortcuts)) {
-    const tr = $(t,'tr');
-    $(tr,'td').textContent = `[${k}]`;
-    const a = $(tr,'td','a');
-    a.textContent = v[1];
-    a.href = v[0].replace(/\/u\/\d\//,`/u/${u}/`);
+    $(table,'tr',
+      $$('td',{ text: `[${k}]` }),
+      $$('td','a',{ href: v[0], text: v[1] })
+    );
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  { // time and date
-    const clock = _id('clock');
-    const date  = _id('date');
-    function refresh_time_1() { return time(clock,date); }
-    (function refresh_time() {
-      setTimeout(refresh_time,refresh_time_1());
-    })();
-  }
-  // { // motd
-  //   fetch('http://otterness-pi.cs.unc.edu:12345/random')
-  //   .then(r => r.text())
-  //   .then(data => { _id('motd').textContent = data; });
-  // }
-  { _id('browser').textContent
-      = navigator.userAgent;
-      // = navigator.userAgent.match(/(?<=^|\s)(Chrom\S+\/\S+)/)[1];
-  }
+  clock = $('clock');
+  date  = $('date');
+  (function refresh_time() { setTimeout(refresh_time, time()); })();
+  $('browser').textContent = navigator.userAgent;
   make_shortcuts();
+  make_work();
 });
 
 document.addEventListener("keypress", e => {
-  /* if (e.key==='j') {
-    for (const x of [
-      'https://mail.google.com/mail/u/0/#inbox',
-      'https://mail.google.com/mail/u/1/#inbox',
-      'https://calendar.google.com/calendar/u/1/r/week'
-    ]) window.open(x,'_blank');
-    window.close();
-  } else */
   if (e.key==='?') {
     navigator.clipboard.readText().then( x => {
-      window.location.href =
-        'https://www.google.com/search?q='+encodeURIComponent(x);
+      window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(x);
     });
   } else {
     const x = shortcuts[e.key];
-    if (x) window.location.href = x[0].replace(/\/u\/\d\//,`/u/${u}/`);
-    else if (['0','1'].includes(e.key)) {
-      if (e.key!==u) {
-        u = e.key;
-        make_shortcuts();
-      }
-    }
+    if (x) window.location.href = x[0];
   }
 });
